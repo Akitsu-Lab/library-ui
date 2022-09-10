940a976b641ccc14a8e17db4478cc4e21105f615
SOTA <snssota@gmail.com>
2022-09-10 19:21:21 +0900

    initial commit

---
+++ src/test/java/com/example/libraryui/LibraryUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
