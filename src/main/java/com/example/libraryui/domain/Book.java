4ffbcbbfa06d12c52eaba4fb993bde6d686ed23c
SOTA <snssota@gmail.com>
2022-09-10 23:06:05 +0900

    domain作成

---
+++ src/main/java/com/example/libraryui/domain/Book.java
@@ -0,0 +1,54 @@
+package com.example.libraryui.domain;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Book {
+
+    private long bookId;
+    @NotEmpty(message = "タイトルを空文字にできません")
+    private String bookTitle;
+    @NotEmpty(message = "ページ数は空文字にできません")
+    private int bookPages;
+    private String bookContent;
+
+    public Book(){}
+    public Book(long bookId, String bookTitle, int bookPages, String bookContent) {
+        this.bookId = bookId;
+        this.bookTitle = bookTitle;
+        this.bookPages = bookPages;
+        this.bookContent = bookContent;
+    }
+
+    //getterとsetter
+    public long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public int getBookPages() {
+        return bookPages;
+    }
+
+    public void setBookPages(int bookPages) {
+        this.bookPages = bookPages;
+    }
+
+    public String getBookContent() {
+        return bookContent;
+    }
+
+    public void setBookContent(String bookContent) {
+        this.bookContent = bookContent;
+    }
+}
---
+++ src/main/java/com/example/libraryui/domain/BookList.java
@@ -0,0 +1,16 @@
+package com.example.libraryui.domain;
+
+import java.util.List;
+
+public class BookList {
+    private List<Book> bookList;
+    
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+}
