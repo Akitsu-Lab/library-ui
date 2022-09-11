a549bd1445a8b886d4fb316900fea46e2084fdb0
SOTA <snssota@gmail.com>
2022-09-11 13:03:29 +0900

    controllerにeditメソッドを追加

--- src/main/java/com/example/libraryui/controller/UiBookController.java
+++ src/main/java/com/example/libraryui/controller/UiBookController.java
@@ -54,4 +54,24 @@ public class UiBookController {
         return "redirect:list";
     }
 
+    @GetMapping("edit")
+    public String edit(@RequestParam long bookId, Model model) {
+        Book book = this.bookDao.get(bookId);
+        model.addAttribute("book", book);
+        model.addAttribute("title", "本更新");
+        return "books/edit";
+    }
+
+    @PostMapping("edit")
+    public String edit(@Validated Book book,
+                       BindingResult result,
+                       Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("pageTitle", "本更新");
+            return "books/edit";
+        }
+        this.bookDao.set(book);
+        return "redirect:list";
+    }
+
 }
